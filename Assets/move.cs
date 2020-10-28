859cebad660f4686ab95bc7dd406c184ab72e3ee
Mokomaki <grest.hyber@gmail.com>
2020-10-28 10:52:27 +0200

    player

---
+++ Assets/move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Camera cam;
+    float min = -5;
+    float max = 5;
+    Rigidbody m_rb;
+
+    public float m_speed = 1f;
+
+    void Start()
+    {
+        cam = Camera.main;
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            m_rb.velocity = (new Vector3(Mathf.Clamp(-(transform.position - cam.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, cam.transform.position.z))).x,min,max), m_rb.velocity.y,0));
+        }
+   
+    }
+}
